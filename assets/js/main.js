0290218c9aba9586afef68e6d82d302aa3c396c5
David Carrera del Castillo <dcarrera@andared.ced.junta-andalucia.es>
2017-05-25 14:57:18 +0200

    Modificación plantilla a CV

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,82 @@
 jQuery(document).ready(function($) {
 
 
-    /*======= Skillset *=======*/
-    
-    
-    $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
+  /*======= Skillset *=======*/
 
-        $('.level-bar-inner').each(function() {
-        
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
-        });
+
+  $('.level-bar-inner').css('width', '0');
+
+  $(window).on('load', function() {
+
+    $('.level-bar-inner').each(function() {
+
+      var itemWidth = $(this).data('level');
+
+      $(this).animate({
+        width: itemWidth
+      }, 800);
 
     });
-   
-    
+
+  });
+
+
+  /*
+   * This is the plugin
+   */
+  (function(a) {
+    a.createModal = function(b) {
+      defaults = {
+        title: "",
+        message: "Your Message Goes Here!",
+        closeButton: true,
+        scrollable: false
+      };
+      var b = a.extend({}, defaults, b);
+      var c = (b.scrollable === true) ? 'style="max-height: 800px;overflow-y: auto;"' : "";
+      html = '<div class="modal fade" id="myModal">';
+      html += '<div class="modal-dialog">';
+      html += '<div class="modal-content">';
+      html += '<div class="modal-header">';
+      html += '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';
+      if (b.title.length > 0) {
+        html += '<h4 class="modal-title">' + b.title + "</h4>"
+      }
+      html += "</div>";
+      html += '<div class="modal-body" ' + c + ">";
+      html += b.message;
+      html += "</div>";
+      html += '<div class="modal-footer">';
+      if (b.closeButton === true) {
+        html += '<button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>'
+      }
+      html += "</div>";
+      html += "</div>";
+      html += "</div>";
+      html += "</div>";
+      a("body").prepend(html);
+      a("#myModal").modal().on("hidden.bs.modal", function() {
+        a(this).remove()
+      })
+    }
+  })(jQuery);
+
+  /*
+   * Here is how you use it
+   */
+  $(function() {
+    $('.view-pdf').on('click', function() {
+      var pdf_link = $(this).attr('href');
+      var iframe = '<div class="iframe-container"><iframe src="' + pdf_link + '"></iframe></div>'
+      $.createModal({
+        title: '',
+        message: iframe,
+        closeButton: false,
+        scrollable: true
+      });
+      return false;
+    });
+  })
+
 
 });
